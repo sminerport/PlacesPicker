66b60b138f87c34e8d57cce53a432beffef08455
Scott <scott.miner.data.scientist@gmail.com>
2025-07-09 08:19:25 -0600

    Implement error handling for user places management; enhance user experience with error modals and place removal functionality

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, map, throwError, tap } from 'rxjs';
 
 import { Place } from './place.model';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private errorService = inject(ErrorService);
   private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
 
@@ -46,12 +48,28 @@ export class PlacesService {
       .pipe(
         catchError((error) => {
           this.userPlaces.set(prevPlaces);
+          this.errorService.showError('Failed to store selected place.');
           return throwError(() => new Error('Failed to store selected place.'));
         })
       );
   }
 
-  removeUserPlace(place: Place) {}
+  removeUserPlace(place: Place) {
+    const prevPlaces = this.userPlaces();
+    if (prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set(prevPlaces.filter((p) => p.id !== place.id));
+    }
+
+    return this.httpClient.delete('http://localhost:3000/user-places/' + place.id)
+    .pipe(
+      catchError((error) => {
+        this.userPlaces.set(prevPlaces);
+        this.errorService.showError('Failed to remove selected place.');
+        return throwError(() => new Error('Failed to remove selected place.'));
+      })
+
+    );
+  }
 
   private fetchPlaces(url: string, errorMessage: string) {
     return this.httpClient.get<{ places: Place[] }>(url).pipe(
--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -35,4 +35,13 @@ export class UserPlacesComponent implements OnInit {
       subscription.unsubscribe();
     });
   }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+
+  }
 }
