f561d003e4bb64e88f03de8e22b7f67c0d55eb73
Scott <scott.miner.data.scientist@gmail.com>
2025-07-09 06:51:14 -0600

    Refactor places management and enhance error handling in available and user places components

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,9 @@
 import { Component, signal, OnInit, inject, DestroyRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -14,18 +14,38 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
-
-  private httpClient = inject(HttpClient);
+  isFetching = signal(false);
+  error = signal('');
+  private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-    const subscription = this.httpClient
-      .get<{ places: Place[] }>('http://localhost:3000/places')
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadAvailablePlaces().subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    const subscription = this.placesService
+      .addPlaceToUserPlaces(selectedPlace.id)
       .subscribe({
         next: (resData) => {
-          console.log(resData.places);
+          console.log(resData);
         },
       });
+
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
